7bbb848c3b1986858c0232623d80280cc0e5f44c
lieby weintraub <liebahinda@gmail.com>
2019-07-24 23:09:06 +0000

    Automatically backed up by Learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,13 +1,14 @@
-class StoreAdminController  < ApplicationController
-layout "admin"
-  def home 
-    
-  end 
-  def orders 
+class StoreAdminController < ApplicationController
+  layout "admin"
+  def home
+
+  end
+  def orders
     render :layout => "order_administration"
-  end 
-  
-  def invoice 
+
+  end
+  def  invoice
     render :layout => false
-  end 
-end 
+
+  end
+end
